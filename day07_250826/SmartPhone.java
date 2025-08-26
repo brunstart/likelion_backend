51ddf02b6d3d726dca0107dc0174ecddf1e2daca
brunstart <brunstart@gmail.com>
2025-08-26 16:21:05 +0900

    인터페이스, default 메소드, static 메소드

---
+++ day07_250826/SmartPhone.java
@@ -0,0 +1,30 @@
+package day07_250826;
+
+public class SmartPhone implements SmartDevice {
+
+    @Override
+    public void turnOn() {
+        System.out.println("스마트폰 전원 ON");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("스마트폰 전원 OFF");
+    }
+
+    @Override
+    public void reset() {
+        System.out.println("스마트폰 공장 초기화 진행");
+        turnOff();
+        turnOn();
+    }
+
+    public static void main(String[] args) {
+        SmartPhone smartPhone = new SmartPhone();
+        smartPhone.turnOn();
+        smartPhone.turnOff();
+        smartPhone.reset();
+        SmartDevice.showInfo();
+
+    }
+}
