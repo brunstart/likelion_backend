2a639cc9a8dcba7027b5d63a26a51d05f489c6f9
brunstart <brunstart@gmail.com>
2025-08-26 16:24:37 +0900

    Object 메소드

---
+++ day07_250826/Yang.java
@@ -0,0 +1,19 @@
+package day07_250826;
+
+public class Yang {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "name : " + name;
+    }
+}
