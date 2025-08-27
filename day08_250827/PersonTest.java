1ca92026f6aad7ef56ab4d9c83a85b00e4afa613
brunstart <brunstart@gmail.com>
2025-08-27 11:44:01 +0900

    toString, equals, hashcode 오버라이딩 예제 (Object 메소드를 오버라이딩)

---
+++ day08_250827/PersonTest.java
@@ -0,0 +1,15 @@
+package day08_250827;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("양승민");
+        person.setAge(30);
+        person.setAddress("강원도 춘천시");
+
+        System.out.println(person);
+
+        Person yang = new Person("yangseungmin", 30, "강원도 춘천시");
+        System.out.println(yang);
+    }
+}
