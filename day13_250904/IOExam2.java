873a083b2063c165a1c66a209b2cb295185e5f02
brunstart <brunstart@gmail.com>
2025-09-04 14:36:46 +0900

    반복문으로 여러명 정보 저장, FileOutputStream에 true추가해서 아예 덮어씌우는게 아닌 append 되도록 수정

--- day13_250904/IOExam2.java
+++ day13_250904/IOExam2.java
@@ -1,62 +1,61 @@
 package day13_250904;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class IOExam2 {
 //     사용자의 정보 (이름, 전화번호, 주소) 를 입력받아서 파일에 저장하는 메소드
-    public void saveInfo()
-    {
-        String name;
-        String phoneNumber;
-        String address;
-        Scanner sc = new Scanner(System.in);
+public void saveUserInfo(){
+    String name = null;
+    String phoneNumber = null;
+    String address = null;
 
-        try (PrintWriter pw = new PrintWriter(new FileWriter("src/day13_250904/result.txt")))
-        {
-            System.out.print("이름 : ");
-            name = sc.nextLine();
-            pw.println("이름 : " + name);
+    try(
+            PrintWriter pw = new PrintWriter(new FileOutputStream("src/day13_250904/userInfo.txt", true));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    ){
+        for(int count = 0; count < 3; count++){
+            System.out.println("이름 : ");
+            name = br.readLine();
+            System.out.println("전화번호 : ");
+            phoneNumber = br.readLine();
+            System.out.println("주소 : ");
+            address = br.readLine();
 
-            System.out.print("전화번호 : ");
-            phoneNumber = sc.nextLine();
-            pw.println("전화번호 : " + phoneNumber);
-
-            System.out.print("주소 : ");
-            address = sc.nextLine();
-            pw.println("주소 : " + address);
-            pw.println();
-
-        }catch (Exception e)
-        {
-            System.out.println(e.getMessage());
+            pw.println(name + " "+phoneNumber + " "+address);
         }
+
+        pw.close();
+        System.out.println("사용자 정보가 src/day12/userInfo.txt 에 저장되었습니다.");
+
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
     }
+}
 
 //     파일로부터 읽어와서 사용자의 정보를 출력하는 메소드
     public void readInfo()
     {
-        String path = "src/day13_250904/";
-        String file = "result.txt";
-        try (BufferedReader reader = new BufferedReader(new FileReader(path + file))) {
-            while((file = reader.readLine()) != null)
-            {
-                System.out.println(file);
-            }
+        String userInfo = null;
 
-        }catch (Exception e)
+        String path = "src/day13_250904/";
+        String file = "userInfo.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(path + file))) {
+            while((userInfo = reader.readLine()) != null)
+            {
+                System.out.println(userInfo);
+            }
+        }catch (IOException e)
         {
             System.out.println(e.getMessage());
         }
     }
 
-
     public static void main(String[] args) {
         IOExam2 io = new IOExam2();
-        io.saveInfo();
+        io.saveUserInfo();
         io.readInfo();
     }
 }
